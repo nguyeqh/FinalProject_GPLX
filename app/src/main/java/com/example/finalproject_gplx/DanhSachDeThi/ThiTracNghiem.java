c6aaf60f08ef91a84f6f05593068bc2141b14cdb
viktorlee54 <519v0054@student.tdtu.edu.vn>
2022-12-03 08:46:48 +0700

    chuc nang hoc bien bao

---
+++ app/src/main/java/com/example/finalproject_gplx/DanhSachDeThi/ThiTracNghiem.java
@@ -0,0 +1,18 @@
+package com.example.finalproject_gplx.DanhSachDeThi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.example.finalproject_gplx.R;
+
+import java.util.Objects;
+
+public class ThiTracNghiem extends AppCompatActivity {
+
+
+}
